c3f24891bc72d07ea7f0c01dc1d4d543790cd229
foon <foon@buzzwoo.de>
2018-03-12 14:59:52 +0700

    add omisego and demo graph feature

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
-
+    public function index ()
+    {
+        return view('index', [
+            'data' => DB::table('prices')->where('coin_id', '=', 6)->get()
+        ]);
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,35 @@
     </head>
 
     <body>
-        {{ $test }}
+        <div style="width:500px;">
+            <canvas id="myChart" width="1600" height="400"></canvas>
+        </div>
     </body>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js"></script>
+    <script>
+        var data = {!! json_encode($data) !!};
+        var priceArray = [];
+        for (var key in data) {
+            priceArray.push(data[key]['price']);
+        };
+
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['1', '2', '3', '4', '5'],
+                datasets: [
+                    {
+                        label: "OmiseGo",
+                        data: priceArray,
+                        borderColor: "red"
+                    }
+                ]
+            },
+            options: {
+                responsive: true
+            }
+        });
+    </script>
 </html>
